8b18d17baf0f30bd1fdbab69ec1bdf85d19784d2
fac3m4n <akerimberdi@gmail.com>
2024-11-03 12:33:00 +0200

    setup

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -19,11 +19,17 @@ export const menuLinks: HeaderMenuLink[] = [
     label: "Home",
     href: "/",
   },
-
   {
-    label: "Debug Contracts",
-    href: "/debug",
-    icon: <BugAntIcon className="h-4 w-4" />,
+    label: "Achievements",
+    href: "/achievements",
+  },
+  {
+    label: "Matchmaking",
+    href: "/matchmaking",
+  },
+  {
+    label: "Profile",
+    href: "/profile",
   },
 ];
 
@@ -94,8 +100,7 @@ export const Header = () => {
             <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-ETH</span>
-            <span className="text-xs">Ethereum dev stack</span>
+            <span className="font-bold leading-tight">GamersDAO</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
